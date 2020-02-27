d3ed5eaa52df0041120403e3ed5973067c4a91b7
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2020-02-26 23:19:55 -0600

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,67 @@
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Curso de Docker ADC',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: 'Material de apoyo para el curso',
+
+  base: '/docker-fundamentals/',
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'og:image', content: 'https://imagesadc.s3.amazonaws.com/eventos/VueLaptop.png' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Basics',
+        link: '/basics/',
+      },
+      {
+        text: 'Github',
+        link: 'https://github.com/RaulNovelo/curso-vuejs-adc'
+      },
+      {
+        text: 'ADC',
+        link: 'http://ascodecodigo.com/'
+      }
+    ],
+    sidebar: {
+      '/basics/': [
+        {
+          title: 'Docker basics',
+          collapsable: false,
+          children: [
+            ''
+          ]
+        }
+      ]
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ docs/README.md
@@ -0,0 +1,11 @@
+---
+meta:
+  - name: keywords
+    content: docker
+home: true
+heroImage: https://imagesadc.s3.amazonaws.com/eventos/DockerLaptop.png
+tagline: ''
+actionText: Quick start →
+actionLink: /basics/
+footer: Made by ADC with ❤️ | Copyright © 2020
+---
